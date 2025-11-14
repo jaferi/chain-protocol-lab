4e86892970bef0344239078ddd9de491852879d1
Hesam Jaferi <hesam.jaferi@gmail.com>
2025-11-14 06:36:30 -0500

    Adding Gossip messaging

--- p2p/src/peer.rs
+++ p2p/src/peer.rs
@@ -20,7 +20,8 @@ pub struct LocalPeer {
 pub struct Node {
     pub local: LocalPeer,
     pub peer_id: PeerId,
-    
+    pub known_peers: Vec<PeerId>,
+    pub received_msgs: Vec<String>   
 }
 
 impl LocalPeer {
@@ -58,6 +59,10 @@ impl Node {
     pub fn new() -> Self {
         let local = LocalPeer::generate();
         let peer_id = local.peer_id();
-        Node {local, peer_id}
+        
+        let known_peers: Vec<PeerId> = vec![];
+        let received_msgs: Vec<String> = vec![];
+
+        Node {local, peer_id, known_peers, received_msgs}
     }
 }
